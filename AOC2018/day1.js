449a935872fc8b90df1308cb271ca7272f1dde23
mgrass2607 <mgrass2607@gmail.com>
2025-03-04 10:59:38 +0000

    This is the stupidest code I've written in a while, but hey it "works"...

--- AOC2018/day1.js
+++ AOC2018/day1.js
@@ -1,13 +1,14 @@
 import '../readfile.js';
 import readFile from '../readfile.js';
-var c = 0;
+var c = new Set([0]);
 var frequency = 0;
-const input = readFile('day1Input.txt').split('\n');
-
+const input = readFile('day1Input.txt').split('\n').map(Number);
+while(true) {
 for (let i in input) {
-  let inp = parseInt(input[i]);
-  frequency += inp
+  frequency += input[i];
+  if (c.has(frequency)) { console.log(frequency); process.exit(); }
+  c.add(frequency);
 }
-
+}
 
 console.log(frequency);
