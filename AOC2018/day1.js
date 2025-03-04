fd892daa260fc1c5ff44b4e34aa27cd46aef5ca2
May1a <mgrass2607@icloud.com>
2025-03-04 09:12:06 +0100

    Even more elegant for loop

--- AOC2018/day1.js
+++ AOC2018/day1.js
@@ -1,11 +1,12 @@
 import '../readfile.js';
 import readFile from '../readfile.js';
-
+var c = 0;
 var frequency = 0;
 const input = readFile('day1Input.txt').split('\n');
 
-for (let i = 0; i < input.length; i++) {
-  frequency += parseInt(input[i])
+for (let i in input) {
+  let inp = parseInt(input[i]);
+  frequency += inp
 }
 
 
