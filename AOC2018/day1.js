4cf150363c9c1de92f287cd94d074fda92a19131
mgrass2607 <mgrass2607@gmail.com>
2025-03-05 21:39:41 +0000

    Revert "create day2 stuff"
    This reverts commit 65bb1c56be6e40cb1efe5bbd807358d1c467430e.

--- AOC2018/day1.js
+++ AOC2018/day1.js
@@ -1,16 +1,14 @@
+import '../readfile.js';
 import readFile from '../readfile.js';
-var c = [];
+var c = new Set([0]);
 var frequency = 0;
-let freqArray = [0];
-const input = readFile('day1Input.txt').split('\n');
-
+const input = readFile('day1Input.txt').split('\n').map(Number);
+while(true) {
 for (let i in input) {
-  let inp = parseInt(input[i]);
-  frequency += inp;
-  if (freqArray.includes(frequency)) {
-    console.log(frequency, freqArray);
-  }
-  freqArray.push(frequency);
+  frequency += input[i];
+  if (c.has(frequency)) { console.log(frequency); process.exit(); }
+  c.add(frequency);
+}
 }
 
 console.log(frequency);
